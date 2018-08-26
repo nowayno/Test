b4e3008b739a4ef16e4e87c5246fc453336b77f2
crazyoung <yangfeng666@sina.cn>
2018-08-26 23:27:32 +0800

    添加项目文件。

---
+++ ConsoleApp1/BookContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BookContext : DbContext
+    {
+        private const string ConnectString = 
+            @"Data Source=(localdb)\ProjectsV13;Initial Catalog=book;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public BookContext() : base(ConnectString) {}
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private async Task AddBookAsync(string title, string publisher)
+        {
+            using (var context = new BookContext())
+            {
+                var book = new Book() { Title = title, Publisher = publisher };
+                context.Books.Add(book);
+                int records = await context.SaveChangesAsync();
+                Console.WriteLine($"{records} record added");
+            }
+            Console.WriteLine();
+        }
+        private async Task AddBooksAsync()
+        {
+            using (var context = new BookContext())
+            {
+                List<Book> list = new List<Book>()
+                {
+                    new Book() { Title = "little", Publisher = "the world" },
+                    new Book() { Title = "big", Publisher = "ll" },
+                    new Book() { Title = "oh", Publisher = "yy" },
+                    new Book() { Title = "my", Publisher = "xx" }
+                };
+
+                context.Books.AddRange(list);
+                int records = await context.SaveChangesAsync();
+                Console.WriteLine($"{records} record added");
+            }
+            Console.WriteLine();
+        }
+        private void ReadBooks()
+        {
+            using (var context = new BookContext())
+            {
+                var books = context.Books;
+                var b = from r in books
+                        where r.Title == "mm"
+                        select r;
+                foreach(var book in b)
+                {
+                    Console.WriteLine($"{book.Title} and {book.Publisher}");
+                }
+                Console.ReadKey();
+            }
+        }
+        private async Task UpdateAsync(string title, string pulisher)
+        {
+            using (var context = new BookContext())
+            {
+                int records = 0;
+                var books = context.Books;
+                var book = (from b in books
+                           where b.Title == "mm"
+                           select b).FirstOrDefault();
+                if(book != null)
+                {
+                    book.Title = "xx";
+                    records = await context.SaveChangesAsync();
+                }
+                Console.WriteLine($"{records} record added");
+            }
+            Console.ReadKey();
+        }
+        private async Task DeleteAsync()
+        {
+            using (var context = new BookContext())
+            {
+                int records = 0;
+                var books = context.Books;
+                context.Books.RemoveRange(books);
+                records = await context.SaveChangesAsync();
+
+                Console.WriteLine($"{records} record added");
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //p.AddBookAsync("mm", "nn").Wait();
+            //p.AddBooksAsync().Wait();
+            //p.UpdateAsync("xx", "ss").Wait();
+            //p.ReadBooks();
+            p.DeleteAsync().Wait();
+
+        }
+    }
+}
